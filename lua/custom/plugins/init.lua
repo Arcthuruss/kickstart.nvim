68c85f5433bc7ae8f818908815823701ce09d867
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2024-10-07 13:17:05 +0200

    changed plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'windwp/nvim-ts-autotag',
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+  },
+}
